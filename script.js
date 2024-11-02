161660efaed30374ab78870b6c692248873ff4ad
Julien-DRANE <183376865+Julien-DRANE@users.noreply.github.com>
2024-11-02 17:22:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -151,16 +151,17 @@ function startOxygenDepletion() {
  * Fonction pour gérer les bips sonores de l'oxygène
 /* ----------------- Ajout des fonctions pour les bips sonores et le son de fin ----------------- */
 
+let gameOverSoundPlayed = false; // Variable pour suivre si le son de fin a été joué
+
 function handleBeepSounds() {
     if (oxygenLevel > 10) {
         if (Math.floor(oxygenLevel) % 10 === 0 && Math.abs(oxygenLevel % 10) < 0.1) {
             beepSound.play();
         }
     } else if (oxygenLevel <= 10 && oxygenLevel > 0) {
-        if (!lowOxygenSoundPlayed) {
+        if (Math.floor(oxygenLevel) === oxygenLevel) {
             lowOxygenSound.load(); // Préparez le son à être joué
             lowOxygenSound.play();
-            lowOxygenSoundPlayed = true; // Marquer le son comme joué pour éviter de le rejouer constamment
         }
     }
 }
